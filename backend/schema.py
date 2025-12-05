f81541f35a94f494bfaba9e1198baa9f30a198d2
Mike1ife <yitai77@gmail.com>
2025-12-04 19:57:56 -0500

    Fix: make Food.servingSize float

--- backend/schema.py
+++ backend/schema.py
@@ -14,7 +14,7 @@ class User(BaseModel):
 
 class Food(BaseModel):
     foodName: Optional[str] = None
-    servingSize: Optional[str] = "1 serving"
+    servingSize: Optional[float]
     calories: Optional[float] = None
     carbohydrate: float
     protein: float
